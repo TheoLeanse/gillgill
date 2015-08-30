20d711f21329c27bbbbb07479259b13a81136c99
TheoLeanse <theoleanse@gmail.com>
2015-08-30 08:01:34 +0100

    use SASS lighten and darken for links

--- config.rb
+++ config.rb
@@ -52,10 +52,5 @@ else
     deploy.remote         = 'https://github.com/TheoLeanse/gillgill.git'
     deploy.branch         = 'gh-pages'
     deploy.commit_message = 'deploying test site'
-    # Optional Settings
-    # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-    # deploy.branch   = 'custom-branch' # default: gh-pages
-    # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-    # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
   end
 end
